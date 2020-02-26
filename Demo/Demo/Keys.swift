055f4c3167d7c316dcfdf820e2846de39f3d27e4
Kushagra Sinha <kushagra@getwalk.in>
2020-02-26 09:57:21 +0530

    Updated Demo

---
+++ Demo/Demo/Keys.swift
@@ -0,0 +1,17 @@
+//
+//  Keys.swift
+//  Demo
+//
+//  Created by Kushagra on 26/02/20.
+//  Copyright © 2020 First Walkin Technologies. All rights reserved.
+//
+
+import Foundation
+
+struct Keys {
+    struct PreferencesKeys {
+        static let AUTH_KEY = "NearXAuthKey"
+        static let MOBILE_NUMBER = "NearXMOBILE_NUMBER"
+        static let NAME = "NearXName"
+    }
+}
